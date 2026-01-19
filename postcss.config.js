f25e3d843f291b425578d90631446acd45298f65
Mansoor Khan <mansoorkhan@192.168.1.29>
2026-01-20 01:30:22 +0500

    Fix: Revert postcss.config.js to plain object (Next.js requirement)

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-module.exports = (ctx) => {
-  // Skip PostCSS processing for node_modules CSS files
-  if (ctx.file && ctx.file.includes('node_modules')) {
-    return {
-      plugins: [],
-    };
-  }
-  
-  return {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  };
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }; 
